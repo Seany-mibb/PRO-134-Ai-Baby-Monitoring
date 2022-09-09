6f1900a8d89537fd5c509819ce99a98484a5121d
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-09-08 17:17:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ cry = '';
 
 function preload()
 {
-
+    cry = loadSound("baby_crying.mp3")
 }
 
 function setup()
@@ -36,30 +36,39 @@ function draw()
         objectDetector.detect(video, gotResult);
         for(i = 0; i < objects.length; i++)
         {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("baby_status").innerHTML = "Scanning...";
+            percent = floor(objects[i].confidence * 100)
+            fill(r, g, b)
+            textSize(18)
+            textFont("segoe UI")
+            text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+30);
+            noFill();
+            stroke(r, g, b)
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
             if(objects[i].label == 'person')
             {
-                document.getElementById("status").innerHTML = "Status: Objects Detected";
-                document.getElementById("baby_status").innerHTML = "The baby is found.";
-                percent = floor(objects[i].confidence * 100)
-                fill(r, g, b)
-                textSize(18)
-                textFont("segoe UI")
-                text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+30);
-                noFill();
-                stroke(r, g, b)
-                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+                cry.stop();
+                document.getElementById("baby_status").innerHTML = "The Baby is found.";
             }
             else
             {
                 document.getElementById("baby_status").innerHTML = "The baby isn't found.";
-                //cry.play();
+                if(!cry.isPlaying())
+                {
+                    cry.play();
+                }
             }
         }
-    }
-
-    if(objects.length = 0)
-    {
-        
+        if(objects.length == 0)
+        {
+            document.getElementById("baby_status").innerHTML = "The baby isn't found.";
+            if(!cry.isPlaying())
+            {
+                cry.play();
+            }
+        }
     }
 }
 
@@ -76,5 +85,6 @@ function gotResult(error, results)
         console.log(error)
     }
     console.log(results)
+    console.log(objects.length);
     objects = results;
 }
